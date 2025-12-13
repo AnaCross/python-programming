43286b500d64bdfe177b1d70fcca620386a0be27
wgrel <w.grela.uczelnia@gmail.com>
2025-12-13 14:48:38 +0100

    Zadanie po formatowaniu.

--- zad_2.py
+++ zad_2.py
@@ -1,26 +1,30 @@
-#2a
+# 2a
 def print_names(names):
     for name in names:
         print(name)
 
-#2b
+
+# 2b
 def print_number_2_for(numbers):
     numbers_multi = []
     for number in numbers:
         numbers_multi.append(number * 2)
     return numbers_multi
 
+
 def print_number_2_list(numbers):
     numbers_multi = [number * 2 for number in numbers]
     return numbers_multi
 
-#2c
+
+# 2c
 def print_elv_numbers(numbers):
     for number in numbers:
         if number % 2 == 0:
             print(number)
 
-#2d
+
+# 2d
 def print_second_numbers(numbers):
     for i in range(1, len(numbers), 2):
         print(numbers[i])
@@ -38,7 +42,3 @@ print(numbers_multi)
 print_elv_numbers(numbers)
 print("kitku")
 print_second_numbers(numbers)
-
-
-
-
